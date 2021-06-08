789cedc67bedabc8ea32f0bae30ef1cdca5f6859
Jip1912 <jip1912@hotmail.com>
2021-06-08 13:36:14 +0200

    Commented some things out in mollieorder.dart

--- lib/src/mollieorder.dart
+++ lib/src/mollieorder.dart
@@ -5,50 +5,56 @@ import 'dart:convert';
 
 class MollieOrderRequest {
   MollieAmount amount;
-  MollieAddress shippingAddress;
-  MollieAddress billingAddress;
-  dynamic metaData;
-  String consumerDateOfBirth;
+  // MollieAddress shippingAddress;
+  // MollieAddress billingAddress;
+  // dynamic metaData;
+  // String consumerDateOfBirth;
   String locale;
   String redirectUrl;
   String webhookUrl;
-  List<MollieProductRequest> products;
+  String description;
+  // List<MollieProductRequest> products;
   String method;
-  String orderNumber;
+  // String orderNumber;
 
   MollieOrderRequest(
       {this.amount,
-      this.billingAddress,
-      this.shippingAddress,
-      this.metaData,
-      this.consumerDateOfBirth,
+      // this.billingAddress,
+      // this.shippingAddress,
+      // this.metaData,
+      // this.consumerDateOfBirth,
       this.locale,
       this.webhookUrl,
       this.redirectUrl,
-      this.products,
+      this.description,
+      // this.products,
       this.method,
-      this.orderNumber});
+      //this.orderNumber
+      });
 
   dynamic toJson() {
-    List<dynamic> productMaps = [];
+    // List<dynamic> productMaps = [];
 
-    for (MollieProductRequest p in products) {
-      print(p.toMap());
-      productMaps.add(p.toMap());
-    }
+    // if(products != null) {
+    //   for (MollieProductRequest p in products) {
+    //     print(p.toMap());
+    //     productMaps.add(p.toMap());
+    //   }
+    // }
 
     return json.encode({
       "amount": amount.toMap(),
-      "billingAddress": billingAddress.toMap(),
-      "shippingAddress": shippingAddress.toMap(),
+      // "billingAddress": billingAddress.toMap(),
+      // "shippingAddress": shippingAddress.toMap(),
       //"metaData": metaData,
-      "consumerDateOfBirth": consumerDateOfBirth,
+      // "consumerDateOfBirth": consumerDateOfBirth,
       "locale": locale,
       "redirectUrl": redirectUrl,
       "webhookUrl": webhookUrl,
-      "orderNumber": orderNumber,
-      "method": method,
-      "lines": productMaps
+      "description": description
+      // "orderNumber": orderNumber,
+      // "method": method,
+      // "lines": productMaps
     });
   }
 }
@@ -56,10 +62,10 @@ class MollieOrderRequest {
 class MollieOrderResponse {
   String id;
   MollieAmount amount;
-  MollieAddress shippingAddress;
-  MollieAddress billingAddress;
-  dynamic metaData;
-  String consumerDateOfBirth;
+  // MollieAddress shippingAddress;
+  // MollieAddress billingAddress;
+  // dynamic metaData;
+  // String consumerDateOfBirth;
   String locale;
   String redirectUrl;
   String webhookUrl;
@@ -74,29 +80,29 @@ class MollieOrderResponse {
 
   MollieOrderResponse.build(dynamic data) {
     id = data["id"];
-
+    print("data: $data");
     amount = MollieAmount(
         currency: data["amount"]["currency"], value: data["amount"]["value"]);
 
-    shippingAddress = MollieAddress.build(data["shippingAddress"]);
-    billingAddress = MollieAddress.build(data["billingAddress"]);
+    // shippingAddress = MollieAddress.build(data["shippingAddress"]);
+    // billingAddress = MollieAddress.build(data["billingAddress"]);
 
     createdAt = data["createdAt"];
     expiresAt = data["expiredAt"];
 
     mode = data["mode"];
 
-    metaData = data["metaData"];
+    // metaData = data["metaData"];
 
-    consumerDateOfBirth = data["consumerDateOfBirth"];
+    // consumerDateOfBirth = data["consumerDateOfBirth"];
     locale = data["locale"];
 
     redirectUrl = data["redirectUrl"];
     webhookUrl = data["webhookUrl"];
 
-    for (int i = 0; i < data["lines"].length; i++) {
-      products.add(new MollieProductResponse.build(data["lines"][i]));
-    }
+    // for (int i = 0; i < data["lines"].length; i++) {
+    //   products.add(new MollieProductResponse.build(data["lines"][i]));
+    // }
 
     status = data["status"];
     method = data["method"];
